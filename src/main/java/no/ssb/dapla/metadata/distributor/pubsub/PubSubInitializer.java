6bc479338dc3564f6761d9bfa7771f6b9976dcd2
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-02-24 22:17:58 +0100

    Remove delegating pubsub in favor of initializer

--- src/main/java/no/ssb/dapla/metadata/distributor/pubsub/PubSubInitializer.java
+++ src/main/java/no/ssb/dapla/metadata/distributor/pubsub/PubSubInitializer.java
@@ -5,6 +5,15 @@ import io.helidon.config.Config;
 public class PubSubInitializer {
 
     public static PubSub create(Config config) {
-        return new DelegatingPubSub(config);
+        boolean useEmulator = config.get("use-emulator").asBoolean().orElse(false);
+        if (useEmulator) {
+            Config emulatorConfig = config.get("emulator");
+            return new EmulatorPubSub(
+                    emulatorConfig.get("host").asString().get(),
+                    emulatorConfig.get("port").asInt().get()
+            );
+        } else {
+            return new RealPubSub(config);
+        }
     }
 }
