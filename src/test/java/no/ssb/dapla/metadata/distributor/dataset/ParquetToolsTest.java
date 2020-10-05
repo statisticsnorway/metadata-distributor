f06f66d12063cbcf8987e14c1793ce2a51c94ef3
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-10-05 15:04:40 +0200

    Added testfile

--- src/test/java/no/ssb/dapla/metadata/distributor/dataset/ParquetToolsTest.java
+++ src/test/java/no/ssb/dapla/metadata/distributor/dataset/ParquetToolsTest.java
@@ -3,13 +3,18 @@ package no.ssb.dapla.metadata.distributor.dataset;
 import org.apache.avro.Schema;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParquetToolsTest {
 
     @Test
-    void checkAvroSchemaFromFile() {
-
-        Schema avroSchemaFromFile = ParquetTools.getAvroSchemaFromFile("file:///../localstack/bin/testdata/skatt/person/rawdata-2019/1582719098762/skatt-v0.53.parquet");
+    void checkAvroSchemaFromFile() throws URISyntaxException {
+        URL resourceUrl = getClass().getResource("dataset.parquet");
+        Schema avroSchemaFromFile = ParquetTools.getAvroSchemaFromFile(resourceUrl.toURI().getRawPath());
+        System.out.println(avroSchemaFromFile);
     }
 }
